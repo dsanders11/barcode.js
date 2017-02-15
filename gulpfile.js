9291fffb9b5e1161bde74e7ce360c150726704dc
David Sanders <dsanders11@ucsbalum.com>
2017-02-15 06:30:42 -0800

    Don't always run check-format for now

--- gulpfile.js
+++ gulpfile.js
@@ -156,7 +156,7 @@ gulp.task('compile', function() {
 });
 
 gulp.task('all', function(cb) {
-  runSequence('check-format', 'compile', 'test', cb);
+  runSequence('compile', 'test', cb);
 });
 
 gulp.task('default', function(cb) {
