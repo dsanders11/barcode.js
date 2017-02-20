d6c71b5bcb593ed4b2891053bbe7a2b0f0f06602
David Sanders <dsanders11@ucsbalum.com>
2017-02-20 06:09:03 -0800

    QRImage extends LuminanceSource (but his days are numbered)

--- src/w69b/qr/qrimage.js
+++ src/w69b/qr/qrimage.js
@@ -1,7 +1,10 @@
 // (c) 2013 Manuel Braun (mb@w69b.com)
 goog.provide('w69b.qr.QRImage');
+goog.require('w69b.LuminanceSource');
+
 
 goog.scope(function() {
+  var LuminanceSource = w69b.LuminanceSource;
 
   /** @typedef {(Uint8ClampedArray|Uint8Array)} */
   w69b.qr.ImageData;
@@ -11,13 +14,15 @@ goog.scope(function() {
    * @param {number} height image height in pixels.
    * @param {w69b.qr.ImageData} data data array.
    * @constructor
+   * @extends {LuminanceSource}
    */
   w69b.qr.QRImage = function(width, height, data) {
-    this.width = width;
-    this.height = height;
+    goog.base(this, width, height)
+
     this.data = data;
   };
   var QRImage = w69b.qr.QRImage;
+  goog.inherits(QRImage, LuminanceSource);
   var pro = QRImage.prototype;
 
   /**
@@ -27,43 +32,27 @@ goog.scope(function() {
    * @return {number} value.
    */
   pro.get = function(x, y) {
-    return this.data[y * this.width + x];
+    return this.data[y * this.getWidth() + x];
   };
 
   /**
-   * @return {number} width.
-   */
-  pro.getWidth = function() {
-    return this.width;
-  };
-
-  /**
-   * @return {number} height.
-   */
-  pro.getHeight = function() {
-    return this.height;
-  };
-
-  /**
-   * @return {w69b.qr.ImageData} raw data.
+   * @override
    */
   pro.getMatrix = function() {
     return this.data;
   };
 
   /**
-   * @param {number} y index.
-   * @param {Uint8Array=} opt_row pre-allocated.
-   * @return {Uint8Array} row.
+   * @override
    */
   pro.getRow = function(y, opt_row) {
     var row;
-    if (opt_row == null || opt_row.length < this.width)
-      row = new Uint8Array(this.width);
+    if (opt_row == null || opt_row.length < this.getWidth())
+      row = new Uint8Array(this.getWidth());
     else
       row = opt_row;
-    var offset = y * this.width;
-    for (var x = 0; x < this.width; ++x)
+    var offset = y * this.getWidth();
+    for (var x = 0; x < this.getWidth(); ++x)
       row[x] = this.data[offset + x];
     return row;
   };
@@ -75,7 +64,7 @@ goog.scope(function() {
    * @return {number} index in data.
    */
   pro.getIndex = function(x, y) {
-    return this.width * y + x;
+    return this.getWidth() * y + x;
   };
 
   /**
@@ -85,7 +74,7 @@ goog.scope(function() {
    * @param {number} value value to set.
    */
   pro.setValue = function(x, y, value) {
-    this.data[y * this.width + x] = value;
+    this.data[y * this.getWidth() + x] = value;
   };
 
   /**
