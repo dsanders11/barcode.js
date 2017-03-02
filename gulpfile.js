182a92cd0fedd5a9ca0b459cfd5c8d6b05756d3a
David Sanders <dsanders11@ucsbalum.com>
2017-03-02 01:15:35 -0800

    Revamp compilation functionality

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,16 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var del = require('del');
-var debug = require('gulp-debug');
 var format = require('gulp-clang-format');
 var karma = require('karma');
 var concat = require('gulp-concat');
-var size = require('gulp-size');
 var sort = require('gulp-sort');
 var through = require('through2');
 var shader2js = require('./tasks/shader2js');
-var webserver = require('gulp-webserver');
-var closure = require('gulp-closure-compiler');
-var closureDeps = require('gulp-closure-deps');
-var closureList = require('gulp-closure-builder-list');
-var wrap = require('gulp-wrap');
-var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
-var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var closureCompiler = require('google-closure-compiler').gulp(
+  {extraArguments: ['-XX:+TieredCompilation']});
 // Adds gulp tag, and gulp bump tasks,
 // see https://github.com/lfender6445/gulp-release-tasks
 require('gulp-release-tasks')(gulp);
@@ -26,7 +20,6 @@ require('gulp-release-tasks')(gulp);
 
 var PATHS = {
   src: {
-    js: ['src/**/*.js'],
     checkFormat: [
       'src/**/*.js'
     ],
@@ -45,47 +38,50 @@ var PATHS = {
 };
 
 var CLOSURE_CONFIG = {
-  compilerPath: 'node_modules/google-closure-compiler/compiler.jar',
-  tieredCompilation: true,
-  continueWithWarnings: true,
-  compilerFlags: {
-    externs: [
-      'externs/iconv.js'
-    ],
-    generate_exports: true,
-    compilation_level: 'ADVANCED_OPTIMIZATIONS',
-    language_in: 'ECMASCRIPT6_STRICT',
-    language_out: 'ECMASCRIPT5_STRICT',
-    dependency_mode: 'STRICT',
-    hide_warnings_for: 'node_modules/',
-    isolation_mode: 'IIFE',
-    warning_level: 'VERBOSE',
-    jscomp_warning: [
-      'const',
-      'constantProperty',
-      'deprecatedAnnotations',
-      'extraRequire',
-      'functionParams',
-      'globalThis',
-      'lintChecks',
-      'missingProperties',
-      'missingProvide',
-      'missingOverride',
-      'missingRequire',
-      'missingReturn',
-      'nonStandardJsDocs',
-      // 'reportUnknownTypes',
-      'undefinedNames',
-      'undefinedVars',
-      'unknownDefines',
-      'unusedLocalVariables',
-      'unusedPrivateMembers',
-      'uselessCode',
-      'underscore',
-      'visibility'
-    ]
-  }
+  assume_function_wrapper: true,
+  externs: [
+    'externs/iconv.js'
+  ],
+  generate_exports: true,
+  compilation_level: 'ADVANCED_OPTIMIZATIONS',
+  language_in: 'ECMASCRIPT6_STRICT',
+  language_out: 'ECMASCRIPT5_STRICT',
+  dependency_mode: 'STRICT',
+  hide_warnings_for: 'node_modules/',
+  isolation_mode: 'IIFE',
+  warning_level: 'VERBOSE',
+  jscomp_warning: [
+    'const',
+    'constantProperty',
+    'deprecatedAnnotations',
+    'extraRequire',
+    'functionParams',
+    'globalThis',
+    'lintChecks',
+    'missingProperties',
+    'missingProvide',
+    'missingOverride',
+    'missingRequire',
+    'missingReturn',
+    'nonStandardJsDocs',
+    // 'reportUnknownTypes',
+    'undefinedNames',
+    'undefinedVars',
+    'unknownDefines',
+    'unusedLocalVariables',
+    'unusedPrivateMembers',
+    'uselessCode',
+    'underscore',
+    'visibility'
+  ]
 };
+
+var CLOSURE_DEBUG_CONFIG = JSON.parse(JSON.stringify(CLOSURE_CONFIG));
+delete CLOSURE_DEBUG_CONFIG['isolation_mode'];
+CLOSURE_DEBUG_CONFIG['compilation_level'] = 'SIMPLE';
+CLOSURE_DEBUG_CONFIG['formatting'] = 'PRETTY_PRINT';
+CLOSURE_DEBUG_CONFIG['output_wrapper'] = 'self.CLOSURE_NO_DEPS = true;\n%output%';
+
 
 gulp.task('clean', function() {
   return del(['dist', 'build', 'coverage']);
@@ -122,7 +118,6 @@ gulp.task('test', ['buildDebug'], function(done) {
   }, done).start();
 });
 
-
 gulp.task('shader2js', function() {
   return gulp.src(PATHS.src.shaders, {base: 'src'})
     .pipe(sort())
@@ -131,9 +126,7 @@ gulp.task('shader2js', function() {
     .pipe(gulp.dest(PATHS.dst.shaders))
 });
 
-
 gulp.task('watch', ['buildDebug'], function() {
-
   gulp.watch(PATHS.src.shaders.concat(PATHS.src.closure), ['buildDebug']);
   gulp.src('.')
     .pipe(webserver({
@@ -143,82 +136,49 @@ gulp.task('watch', ['buildDebug'], function() {
     }));
 });
 
-
-function buildDebug(module) {
-  var bundleStream = through.obj();
-  var fileList;
-
-  var getFiles = through.obj(function(file, enc, cb) {
-    if (file.isStream()) {
-      cb(new gutil.PluginError('buildDebug', 'Streaming not supported'));
-      return;
-    }
-    fileList = file.contents.toString().split('\n');
-    cb();
-  });
-
-  var es = require('event-stream');
-
-  var str = gulp.src(PATHS.src.closure)
-    .pipe(closureDeps({
-      prefix: '',
-      baseDir: '.'
-    }))
-    .pipe(closureList({
-      entryPoint: module,
-    }))
-    .pipe(getFiles)
-    .on('end', function() {
-      gulp.src([PATHS.src.closureBase].concat(fileList), { base: '.' })
-        .pipe(bundleStream);
-    });
-  return es.duplex(str, es.merge(bundleStream, str));
-}
-
-var closureDebugWrapper = 'self.CLOSURE_NO_DEPS = true;\n<%= contents %>';
-/**
- * Build library simply by concatenating all files.
- */
 gulp.task('buildDebug:main', ['shader2js'], function() {
-  return buildDebug('main')
+  var config = JSON.parse(JSON.stringify(CLOSURE_DEBUG_CONFIG));
+  config['js_output_file'] = 'w69b.qrcode.js';
+  config['entry_point'] = 'main';
+
+  return gulp.src(PATHS.src.closure)
     .pipe(sourcemaps.init())
-    .pipe(concat('w69b.qrcode.js'))
-    .pipe(wrap(closureDebugWrapper))
+    .pipe(closureCompiler(config))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('buildDebug:worker', ['shader2js'], function() {
-  return buildDebug('w69b.worker.DecodeWorker')
+  var config = JSON.parse(JSON.stringify(CLOSURE_DEBUG_CONFIG));
+  config['js_output_file'] = 'w69b.qrcode.decodeworker.js';
+  config['entry_point'] = 'worker';
+
+  return gulp.src(PATHS.src.closure)
     .pipe(sourcemaps.init())
-    .pipe(concat('w69b.qrcode.decodeworker.js'))
-    .pipe(wrap(closureDebugWrapper))
+    .pipe(closureCompiler(config))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('buildDebug', ['buildDebug:main', 'buildDebug:worker']);
 
-
 gulp.task('compile:main', function() {
   var config = JSON.parse(JSON.stringify(CLOSURE_CONFIG));
-  config['fileName'] = 'w69b.qrcode.min.js';
-  config['compilerFlags']['entry_point'] = 'main';
-  config['compilerFlags']['assume_function_wrapper'] = true;
+  config['js_output_file'] = 'w69b.qrcode.min.js';
+  config['entry_point'] = 'main';
 
   return gulp.src(PATHS.src.closure)
-    .pipe(closure(config))
+    .pipe(closureCompiler(config))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compile:worker', function() {
   var config = JSON.parse(JSON.stringify(CLOSURE_CONFIG));
-  config['fileName'] = 'w69b.qrcode.decodeworker.min.js';
-  config['compilerFlags']['entry_point'] = 'worker';
-  config['compilerFlags']['assume_function_wrapper'] = true;
+  config['js_output_file'] = 'w69b.qrcode.decodeworker.min.js';
+  config['entry_point'] = 'worker';
 
   return gulp.src(PATHS.src.closure)
-    .pipe(closure(config))
+    .pipe(closureCompiler(config))
     .pipe(gulp.dest('dist'));
 });
 
