377000522d8a3200a75a3e66a59c573e8bb1e290
David Sanders <dsanders11@ucsbalum.com>
2017-05-07 00:33:26 -0700

    Fix indentation

--- src/w69b/qr/encoder/maskutil.js
+++ src/w69b/qr/encoder/maskutil.js
@@ -24,13 +24,13 @@ goog.scope(function() {
   var ByteMatrix = w69b.qr.encoder.ByteMatrix;
   var IllegalArgumentException = w69b.exceptions.IllegalArgumentException;
 
-/**
- * @author Satoru Takabayashi
- * @author Daniel Switkin
- * @author Sean Owen
- * @author mb@69b.com (Manuel Braun) ported to js
- */
-var _ = w69b.qr.encoder.MaskUtil;
+  /**
+   * @author Satoru Takabayashi
+   * @author Daniel Switkin
+   * @author Sean Owen
+   * @author mb@69b.com (Manuel Braun) ported to js
+   */
+  var _ = w69b.qr.encoder.MaskUtil;
 
   // Penalty weights from section 6.8.2.1
   const N1 = 3;
