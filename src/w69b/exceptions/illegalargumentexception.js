937fbc91ee691465592f4c94d69fc2e274dd7c9d
David Sanders <dsanders11@ucsbalum.com>
2017-05-14 10:03:52 -0700

    Make IllegalArgumentException take optional throwable

--- src/w69b/exceptions/illegalargumentexception.js
+++ src/w69b/exceptions/illegalargumentexception.js
@@ -6,11 +6,17 @@ goog.scope(function() {
    * Thrown to indicate that a method has been passed an illegal or
    * inappropriate argument.
    * @constructor
-   * @param {string=} opt_msg message.
+   * @param {(goog.debug.Error|string)=} opt_throwable_or_msg cause or message.
    * @extends {goog.debug.Error}
    */
-  w69b.exceptions.IllegalArgumentException = function(opt_msg) {
-    goog.base(this, opt_msg);
+  w69b.exceptions.IllegalArgumentException = function(opt_throwable_or_msg) {
+    var msg = opt_throwable_or_msg;
+
+    if (opt_throwable_or_msg instanceof goog.debug.Error) {
+      msg = opt_throwable_or_msg.toString();
+    }
+
+    goog.base(this, msg);
   };
   goog.inherits(w69b.exceptions.IllegalArgumentException, goog.debug.Error);
 });
