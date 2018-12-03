cb56ebe1793580018a81e244ec3c0d7671ffdc20
David Sanders <dsanders11@ucsbalum.com>
2018-12-03 06:27:56 -0700

    Migrate IllegalArgumentException to module-style

--- src/java/lang/illegalargumentexception.js
+++ src/java/lang/illegalargumentexception.js
@@ -2,26 +2,28 @@
  * @fileoverview
  * @suppress {duplicate}
  */
+goog.module('java.lang.IllegalArgumentException');
+goog.module.declareLegacyNamespace();
 
-goog.provide('java.lang.IllegalArgumentException');
-goog.require('goog.debug.Error');
+const GoogDebugError = goog.require('goog.debug.Error');
 
-goog.scope(function() {
+/**
+ * Thrown to indicate that a method has been passed an illegal or inappropriate
+ * argument.
+ */
+class IllegalArgumentException extends GoogDebugError {
   /**
-   * Thrown to indicate that a method has been passed an illegal or
-   * inappropriate argument.
-   * @constructor
-   * @param {(!goog.debug.Error|string)=} opt_throwable_or_msg cause or message.
-   * @extends {goog.debug.Error}
+   * @param {(!GoogDebugError|string)=} opt_throwable_or_msg cause or message.
    */
-  java.lang.IllegalArgumentException = function(opt_throwable_or_msg) {
+  constructor(opt_throwable_or_msg) {
     let msg = opt_throwable_or_msg;
 
-    if (opt_throwable_or_msg instanceof goog.debug.Error) {
+    if (opt_throwable_or_msg instanceof GoogDebugError) {
       msg = opt_throwable_or_msg.toString();
     }
 
-    java.lang.IllegalArgumentException.base(this, 'constructor', msg);
-  };
-  goog.inherits(java.lang.IllegalArgumentException, goog.debug.Error);
-});
+    super(msg);
+  }
+}
+
+exports = IllegalArgumentException;
